6dd6c6d33f4aa48084b1e26bb351c63341523d5e
JonathanBaidoo <160986051+JonathanBaidoo@users.noreply.github.com>
2024-03-28 09:19:52 -0500

    Update p.js

---
+++ p.js
@@ -0,0 +1,50 @@
+/* You must use CSS classes for toggling styles, adding or removing classes to change the style. 
+You are not permitted to manually apply styles to elements with JavaScript. 
+
+You must use addEventListener for all your event handling
+
+CSS
+Create a separate CSS file that handles the styling for the page.
+
+Style the buttons so that they sit horizontally inside their container. 
+The four buttons together should take up the full width of the container and each button should be the same width.
+
+Include a 1px black border around button container, and 10px of padding.
+
+There must be a 10px gap between each of the buttons.
+
+The paragraph must have a 3px border of ​#ff2800​ (​Ferrari Red)​ and 15px of padding.*/
+
+
+
+
+button{
+    display: inline;
+    padding-right: 10px;
+}
+
+.button_container{
+    border: 1px solid #000;
+    padding: 10px;
+}
+
+p{
+    border: 3px solid #ff2800;
+    padding: 15px;
+}
+
+.button1{
+    display: bold;
+}
+
+.button2{
+    width: 50%;
+}
+
+.button3{
+    color: #ff2800;
+}
+
+.button4{
+    font-size: 12px;
+}
